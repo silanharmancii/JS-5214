460aac07c6ae298fa3762f91c385d87ec5982262
Şilan HK <silanharmanci.9@gmail.com>
2024-05-19 22:55:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var menu = document.getElementById("myLinks");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
